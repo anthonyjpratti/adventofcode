c3941b1494812fd77cde7a60a046cc811ca587d2
Anthony Pratti <anthonyjpratti@gmail.com>
2019-12-03 21:57:58 -0500

    First solution actually done

--- 2019/1/rs/src/main.rs
+++ 2019/1/rs/src/main.rs
@@ -1,12 +1,12 @@
+use std::error::Error;
 use std::fs;
 use structopt::StructOpt;
-use std::error::Error;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "2019-1", about = "First day of Advent of Code")]
 struct Opt {
-  #[structopt(short = "p", long = "path")]
-  path: String,
+    #[structopt(short = "p", long = "path")]
+    path: String,
 }
 
 fn main() {
@@ -16,22 +16,28 @@ fn main() {
     println!("Solution: {}", output);
 }
 
-
 fn read_file(path: String) -> Result<Vec<u32>, Box<dyn Error>> {
-  let mut res = Vec::new();
-  for line in fs::read_to_string(path)?.split('\n') {
-    if line != "" {
-      let entry = line.parse::<u32>()?;
-      res.push(entry);
+    let mut res = Vec::new();
+    for line in fs::read_to_string(path)?.split('\n') {
+        if line != "" {
+            let entry = line.parse::<u32>()?;
+            res.push(entry);
+        }
     }
-  }
-  Ok(res)
+    Ok(res)
 }
 
 fn determine_fuel_requirements(modules: Vec<u32>) -> u32 {
-  modules.iter().map(calc_fuel_per_module).sum()
+    modules.iter().map(calc_fuel_per_module).sum()
 }
 
 fn calc_fuel_per_module(module: &u32) -> u32 {
-  module / 3 - 2
+    let mut total = 0;
+    let mut curr = *module;
+    while curr > 5 {
+        let temp = curr / 3 - 2;
+        total += temp;
+        curr = temp;
+    }
+    total
 }
